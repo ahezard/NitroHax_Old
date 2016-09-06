4c66fa29d618b10d9422fc5b039ec9042c5ee67f
ahezard <ahezard@gmail.com>
2016-09-07 00:59:16 +0200

    Switch to new libnds dsi_resetSlot1(); function

--- arm7/source/main.c
+++ arm7/source/main.c
@@ -33,32 +33,6 @@ void VcountHandler() {
 void VblankHandler(void) {
 }
 
-void ResetSlot() {
-		
-	// Power off Slot
-	while(REG_SCFG_MC&0x0C !=  0x0C); 		// wait until state<>3
-	if(REG_SCFG_MC&0x0C != 0x08) return; 		// exit if state<>2      
-	
-	REG_SCFG_MC = 0x0C;          		// set state=3 
-	while(REG_SCFG_MC&0x0C !=  0x00);  // wait until state=0
-
-	swiWaitForVBlank();
-
-	// Power On Slot
-	while(REG_SCFG_MC&0x0C !=  0x0C); // wait until state<>3
-	if(REG_SCFG_MC&0x0C != 0x00) return; //  exit if state<>0
-	
-	REG_SCFG_MC = 0x04;    // wait 1ms, then set state=1
-	while(REG_SCFG_MC&0x0C != 0x04);
-	
-	REG_SCFG_MC = 0x08;    // wait 10ms, then set state=2      
-	while(REG_SCFG_MC&0x0C != 0x08);
-	
-	REG_ROMCTRL = 0x20000000; // wait 27ms, then set REG_ROMCTRL=20000000h
-	
-	while(REG_ROMCTRL&0x8000000 != 0x8000000);
-}
-
 //---------------------------------------------------------------------------------
 int main(void) {
 //---------------------------------------------------------------------------------
@@ -93,7 +67,7 @@ int main(void) {
 	irqEnable( IRQ_VBLANK | IRQ_VCOUNT);   
 
 	fifoWaitValue32(FIFO_USER_01);
-	ResetSlot();
+	dsi_resetSlot1();
 	fifoSendValue32(FIFO_USER_02, 1);
 
 	// Keep the ARM7 mostly idle
